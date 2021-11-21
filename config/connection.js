3a38fa69133aaf817b0a48ae0b3bcbbdef49f2f4
Dan <dan.pandolfo@gmail.com>
2021-11-20 21:29:22 -0500

    set up file structure, insalled npm packages

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//importe the sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config()
+
+let sequelize;
+
+if (process.env.JAWSDB_URL){
+  sequelize= new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+// create connection to our db
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+module.exports = sequelize;
