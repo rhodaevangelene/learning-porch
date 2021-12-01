649436c2ba7cab6cd9067de76371024b6be605ab
Dan <dan.pandolfo@gmail.com>
2021-11-30 22:26:52 -0500

    added model updates

---
+++ models/Library.js
@@ -0,0 +1,38 @@
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Library extends Model {}
+
+Library.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        book_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'books',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Library'
+    }
+);
+
+module.exports = Library;
